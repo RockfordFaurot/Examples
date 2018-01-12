51f31b93fe0cc9514d5d7278eb4ebc8a7462a654
Rockford Faurot <rockfordfaurot@Rockfords-MacBook-Pro.local>
2018-01-11 20:05:05 -0600

    adding example 1

---
+++ Github_example1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+	int main()
+	{
+		FILE *ptr_file;
+		char buf[1000];
+		int x;
+
+		printf("opening up file output.text\n");
+		ptr_file =fopen("/Users/rockfordfaurot/Documents/Programing/C/output.txt", "r");
+
+		if (!ptr_file)
+			printf("fopen was unsuccesfull.\n");
+			return 1;
+		while(fgets(buf,1000,ptr_file)!=NULL)
+		{
+			x++;
+		}
+		fclose(ptr_file);
+		printf("The number of lines in the file is: %d\n", x);
+		return  0;
+	}

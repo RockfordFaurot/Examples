4d98d87dea92ec1bc5e92fa189be115e2d1f5b82
Rockford Faurot <rockfordfaurot@Rockfords-MacBook-Pro.local>
2018-01-11 20:55:10 -0600

    fixed code: if statment missing brackets ({  })

--- Github_example1.c
+++ Github_example1.c
@@ -8,14 +8,19 @@
 
 		printf("opening up file output.text\n");
 		ptr_file =fopen("/Users/rockfordfaurot/Documents/Programing/C/output.txt", "r");
-
+		
 		if (!ptr_file)
+		{
 			printf("fopen was unsuccesfull.\n");
 			return 1;
+		}
+		printf("Test\n");
+		
 		while(fgets(buf,1000,ptr_file)!=NULL)
 		{
 			x++;
 		}
+		
 		fclose(ptr_file);
 		printf("The number of lines in the file is: %d\n", x);
 		return  0;

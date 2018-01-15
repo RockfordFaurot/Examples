4707e976b1fd406ebabf95c6a8e8eba5314dcdf2
Rockford Faurot <rockfordfaurot@Rockfords-MacBook-Pro.local>
2018-01-15 15:17:29 -0600

    created aditinal feature to count the number of charictures in each line of text.

--- Github_example1.c
+++ Github_example1.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
+#include<string.h>
 
 	int main()
 	{
 		FILE *ptr_file;
 		char buf[1000];
 		int x;
+		unsigned long length;
+		int holder[10];
 
 		printf("opening up file output.text\n");
 		ptr_file =fopen("/Users/rockfordfaurot/Documents/Programing/C/output.txt", "r");
@@ -19,6 +22,8 @@
 		while(fgets(buf,1000,ptr_file)!=NULL)
 		{
 			x++;
+			length = strlen(buf);
+			printf("the string length is %lu\n", length);
 		}
 		
 		fclose(ptr_file);
